aea5cf75697b52a2c327048b3b8da64170f7c3ae
szopian <szopiangreg@gmail.com>
2021-08-24 00:52:27 +0100

    add work on reuseable button

--- react-modal-app/src/App.js
+++ react-modal-app/src/App.js
@@ -1,12 +1,14 @@
 import { GlobalStyles, PrimaryButton } from "./components/styles";
 import Modal from "./components/Modal.jsx";
 import React, { useState } from "react";
+import Button from "./components/Button";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <GlobalStyles />
+      <Button text="click me" rgbaShadow="24, 90, 219" />
       <PrimaryButton onClick={() => setIsOpen(true)}>Open Modal</PrimaryButton>
       {isOpen && <Modal setIsOpen={setIsOpen} />}
     </>
---
+++ react-modal-app/src/components/Button.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
+const Btn = styled.button`
+  margin: 20px 10px;
+  cursor: pointer;
+  font-weight: 500;
+  padding: 17px 35px;
+  border-radius: 10px;
+  font-size: 0.8rem;
+  border: none;
+  color: white;
+  background: #185adb;
+  transition: all 0.25s ease;
+
+  &&:hover {
+    transform: translateY(-5px);
+    box-shadow: 0 10px 20px -10px rgba(${(props) => props.BtnShadowColor}, 0.6);
+  }
+`;
+
+const Button = ({ text, rgbaShadow }) => {
+  return <Btn BtnShadowColor={rgbaShadow}>{text}</Btn>;
+};
+
+export default Button;
