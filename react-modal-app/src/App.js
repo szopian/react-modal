c7ca528f65c9cf5e31504f79c976387d14ae5942
szopian <szopiangreg@gmail.com>
2021-08-24 20:23:50 +0100

    add changes

--- react-modal-app/src/App.js
+++ react-modal-app/src/App.js
@@ -3,29 +3,22 @@ import Modal from "./components/Modal.jsx";
 import React, { useState } from "react";
 import Button from "./components/Button.jsx";
 
-function setFalse() {
-  return false;
-}
-
-function setTrue() {
-  return true;
-}
-
 function App() {
-  const [isOpen, setIsOpen] = useState(() => setFalse());
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
       <GlobalStyles />
+
       <Button
         text="Open Modal"
         bgColor="#185adb"
         rgbaShadow="24, 90, 219"
-        onClick={() => setIsOpen(() => setTrue())}
+        onClick={() => setIsOpen(true)}
       />
-      <PrimaryButton onClick={() => setIsOpen(() => setTrue())}>
-        Open Modal
-      </PrimaryButton>
+
+      <PrimaryButton onClick={() => setIsOpen(true)}>Open Modal</PrimaryButton>
+
       {isOpen && <Modal setIsOpen={setIsOpen} />}
     </>
   );
